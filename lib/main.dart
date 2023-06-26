b15829a7f1ebd8d45990fc9576495566ca8c06c8
ayomidepeat <peatayomide@gmail.com>
2023-06-26 12:14:02 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myroute/features/PassengerBookingFlow/view/BottomNavigatorBar.dart';
+import 'package:myroute/features/Splash/views/Slash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: "Avenir",
       ),
-      home: BottomNav(),
+      home: Splash1(),
     );
   }
 }
